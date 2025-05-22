0d67ca26aa2d26b36dc0f92bf9d04296ff2c719a
Wilder <w.cossio.r@gmail.com>
2025-05-22 17:58:50 -0400

    finishing the last details, like descriptions projects and introduction

--- src/components/AboutDescription.tsx
+++ src/components/AboutDescription.tsx
@@ -10,7 +10,18 @@ export const AboutDescription = () => {
             <p className='animate__animated animate__fadeInLeft font-normal text-xl'>Frontend Developer</p>
             <div className='flex items-center animate__animated animate__fadeInLeft' style={{ animationDelay: '0.9s' }}>
                 <span className='flex-none font-light text-emerald-400 text-[20rem]'>{'['}</span>
-                <p className='flex-1 text-gray-400'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil est velit enim nam accusantium obcaecati et ad a nulla pariatur? Quia, asperiores odio? Beatae sapiente rem unde minus quas nobis placeat totam expedita libero debitis dolorum eligendi recusandae, repudiandae, nam enim in, vitae asperiores reprehenderit amet consectetur mollitia cupiditate hic distinctio iusto. Laboriosam aliquid pariatur voluptatibus sint nemo mollitia a, facilis explicabo aut delectus quia adipisci cum eligendi accusantium ipsam nihil dicta, impedit, iusto voluptate? Debitis ratione voluptate aperiam vero rem iusto eos voluptatibus culpa, praesentium vel illum pariatur nulla dicta quis odio quas, ipsa assumenda nostrum sit? Asperiores ad obcaecati cupiditate tempora illo earum laudantium saepe, reprehenderit hic deleniti dolorum in et tenetur maxime ea atque? Impedit cum voluptatum nemo. Enim, incidunt porro! Perferendis quaerat odit illo veniam et dolor, suscipit deleniti provident alias nostrum dolores. Alias dolorem molestiae deserunt ipsa id aperiam itaque illo ratione nulla quod dignissimos molestias nostrum quidem delectus nemo at nihil possimus reprehenderit nobis, officiis corrupti! Quia corrupti nisi earum? Et, mollitia, nam perferendis commodi doloremque quisquam inventore molestiae porro repellendus possimus reiciendis. Culpa exercitationem sequi esse tempora quo pariatur assumenda dolor nihil placeat error, in nobis asperiores laborum consectetur enim consequuntur repellendus velit?</p>
+                <pre className='flex-1 text-gray-400 whitespace-pre-wrap'>
+                    👋 Hello! I’m a Frontend developer passionate about creating clean, responsive, and user-centered interfaces.
+                    
+                    I’ve worked on several projects using React, TypeScript, TailwindCSS, and DaisyUI, including a travel website, a movie site, and a task management app. I also built a facial recognition system for personnel tracking, designed to be fully responsive.
+
+                    As part of my graduation project, I developed a cross-platform mobile app for managing tennis tournaments using Flutter, Firebase, and SCRUM. I also designed apps for the Wilstermann Club and a livestock social network.
+
+                    I value clean code, accessibility, and attention to detail. I'm always improving my skills, currently exploring tools like Zustand and refining my UI design with Tailwind and DaisyUI.
+
+                    I enjoy collaborating in teams, receiving feedback, and continuously growing as a developer.
+                </pre>
+
             </div>
         </div>
     )
--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -5,9 +5,10 @@ type ProjectItemProps = {
     shortDescription: string,
     projectLink: string,
     img: string,
+    technologies: string[]
 }
 
-export const ProjectItem = ({ title, shortDescription, projectLink, img }: ProjectItemProps) => {
+export const ProjectItem = ({ title, shortDescription, projectLink, img, technologies }: ProjectItemProps) => {
     const [isHovered, setIsHovered] = useState(false);
 
     const handleMouseEnter = () => {
@@ -22,13 +23,17 @@ export const ProjectItem = ({ title, shortDescription, projectLink, img }: Proje
         <div className="card bg-neutral text-neutral-content">
             <div className={`card-body p-0 items-center text-center transition-all duration-500 ${isHovered ? 'scale-103' : 'scale-100'}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                 <div className={`absolute w-full text-gray-200 rounded-sm h-full bg-gradient-to-br from-gray-900 via-gray-800 to-emerald-500 p-8 text-start transition-all duration-500 ${isHovered ? 'opacity-92' : 'opacity-0'}`}>
-                    <h2 className="card-title">{title}</h2>
-                    <p>{shortDescription}</p>
-                    <div className="card-actions justify-end">
-                        <a
-                            href={projectLink}
-                            target="_blank"
-                            rel="noopener noreferrer" className="btn border-none bg-neutral text-gray-200">Check out</a>
+                    <div className='flex justify-between'>
+                        <h2 className="card-title">{title}</h2>
+                        <a href={projectLink} target="_blank" className="text-emerald-500 hover:underline">
+                            See more →
+                        </a>
+                    </div>
+                    <p className='text-xs my-4'>{shortDescription}</p>
+                    <div className="text-sm flex flex-wrap gap-2 text-gray-400">
+                        {
+                            technologies.map((tech, index) => (<span className="badge badge-outline">{tech}</span>))
+                        }
                     </div>
                 </div>
                 <img src={img} className='w-full h-full object-cover rounded-sm' alt="MinitwitterImg" />
